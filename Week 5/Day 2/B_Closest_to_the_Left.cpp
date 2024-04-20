f4e53485561a8fd223bb8c35e775f53941817e20
Shakil Rahman <121746767+ShakilRahman768@users.noreply.github.com>
2024-04-20 14:58:47 +0600

    Add files via upload

---
+++ Week 5/Day 2/B_Closest_to_the_Left.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, t;
+    cin >> n >> t;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    while (t--)
+    {
+        int m;
+        cin >> m;
+        int l = 0, r = n - 1, mid;
+        int indx = -1;
+        while (l <= r)
+        {
+            mid = (l + r) / 2;
+            if (arr[mid] <= m)
+            {
+                indx = mid;
+                l = mid + 1;
+            }
+            else if (arr[mid] > m)
+            {
+                r = mid - 1;
+            }
+            // else
+            // {
+            //     l = mid + 1;
+            // }
+        }
+        cout << indx + 1 << "\n";
+    }
+    return 0;
+}
