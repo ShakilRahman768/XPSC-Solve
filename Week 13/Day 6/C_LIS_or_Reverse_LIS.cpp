d6d8b09d10a0bcab26234c527cab1be2a809701c
Shakil Rahman <121746767+ShakilRahman768@users.noreply.github.com>
2024-06-27 23:52:47 +0600

    Add files via upload

---
+++ Week 13/Day 6/C_LIS_or_Reverse_LIS.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x;
+        cin >> n;
+        map<int, int> mp;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> x;
+            mp[x]++;
+        }
+        int cnt1 = 0, cnt2 = 0;
+        for (auto it : mp)
+        {
+            if (it.second > 1)
+            {
+                cnt1++;
+                cnt2++;
+            }
+            else
+            {
+                if (cnt1 > cnt2)
+                {
+                    cnt2++;
+                }
+                else
+                {
+                    cnt1++;
+                }
+            }
+        }
+        cnt2++;
+        cout << min(cnt1, cnt2) << "\n";
+    }
+    return 0;
+}
